2d63b9f8dc118cd1e0c8bad7c9167df45eb126dd
ArtilleryTrigger <artillerytrigger@gmail.com>
2021-07-14 16:21:33 +0300

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+const inputTask = document.getElementById("input--task");
+const btn = document.getElementById("btn");
+const task = document.querySelector(".task");
+
+btn.onclick = function (){
+    task.innerHTML += <button></button>
+}
