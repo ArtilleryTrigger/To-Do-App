7c1f844ee176d91ddc7d74180671a17c232157b9
ArtilleryTrigger <artillerytrigger@gmail.com>
2021-07-14 22:43:21 +0300

    init

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const inputTask = document.getElementById("input--task");
-const btn = document.getElementById("btn");
-const task = document.querySelector(".task");
+let inputTask = document.getElementById("input--task");
+let btn = document.getElementById("btn");
+let task = document.getElementById("task");
 
-btn.onclick = function (){
-    task.innerHTML += <button></button>
+btn.addEventListener("click",function(){
+    let outputTask = document.createElement("li");
+    outputTask.classList.add("output-task");
+    task.appendChild(outputTask);
+    outputTask.innerHTML = inputTask.value + '<button class="delete-icon"><i class="fas fa-trash-alt"></i></button>';
+    inputTask.value = "";
+    task.addEventListener("click", deleteItem);
+    outputTask.addEventListener("click", function(){
+        outputTask.classList.toggle('checked');
+        outputTask.style.textDecoration = 'line-through';
+        outputTask.style.backgroundColor = 'green'
+    })
+});
+
+function deleteItem(e){
+    if(e.target.className === 'fas fa-trash-alt'){
+        if(confirm('Are you sure you want to delete')){
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+    e.preventDefault();
 }
